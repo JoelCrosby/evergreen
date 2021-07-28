0d08f089043ae1ff2092b4c8000b1b47746c7ff8
Joel Crosby <joelcrosby@live.co.uk>
2021-07-28 17:07:46 +0100

    add initial project scaffolding

---
+++ Evergreen.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evergreen.App.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
